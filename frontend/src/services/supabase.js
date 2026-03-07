b469d4fbfc2cdcfd211369f4867d1f6a74bcd0a2
kajolNiloy <kajolniloy48@gmail.com>
2026-03-07 13:30:21 +0900

    Fix supabase key

--- frontend/src/services/supabase.js
+++ frontend/src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://yxxmhxfrdubdxrxeylqd.supabase.co';
-const supabaseKey = yxxmhxfrdubdxrxeylqd;
+const supabaseKey = 'yxxmhxfrdubdxrxeylqd';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
